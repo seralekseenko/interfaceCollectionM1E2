43184b92f8795313f88b0607f856fb08aea90e28
seralekseenko <seralekseenko@ukr.net>
2017-03-04 20:31:28 +0000

    try maven

---
+++ interfaceCollectionM1E2/src/main/java/interfaceCollectionM1E2/App.java
@@ -0,0 +1,13 @@
+package interfaceCollectionM1E2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
